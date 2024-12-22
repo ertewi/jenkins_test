0f32c639d8f7d0bb39ca7b8ad797b40b09986ffc
ertewi <60309968+ertewi@users.noreply.github.com>
2024-12-23 04:33:42 +0700

    Update app.js 8

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('12345678 This is a test application.');
+  res.send('1234567 This is a test application.');
 });
 
 app.listen(port, () => {
