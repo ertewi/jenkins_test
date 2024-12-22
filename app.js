ab0c41ee5306af69bfa1496fc91b08a37ce96878
ertewi <nikita28984@gmail.com>
2024-12-23 03:52:57 +0700

    123

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('This is a test application.');
+  res.send('123 This is a test application.');
 });
 
 app.listen(port, () => {
